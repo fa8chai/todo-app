5690455b615e5e38572286aea132aceaacc1014c
fatima <anne.wi.sw@gmail.com>
2019-01-08 15:23:22 +0300

    first commit

--- script.js
+++ script.js
@@ -4,24 +4,78 @@ var thelist = document.getElementById("unlist");
 var theinput = document.getElementById("inputs");
 
 
+
 addbutton.addEventListener("click", function add(){
-    var val = theinput.Value;
-    if (val.trim()){
-        var lich = document.createTextNode(val);
-        var ch = document.createElement("LI");
-        var bu = document.createElement("BUTTON");
-        bu.innerHTML="Finish";
-        ch.appendChild = lich;
-        ch.appendChild = bu;
-        thelist.appendChild(ch);
-        val=""; 
+    var text = theinput.value;
+    if(text.trim()){
+        var thenod = document.createTextNode(text);
+        var child = document.createElement("LI");
+        child.appendChild(thenod);
+        var finish = document.createElement("button");
+        finish.innerHTML="Finish";
+        finish.className="finish";
+        finish.addEventListener("click",function remove(e){
+            var item = e.target.parentElement;
+            thelist.removeChild(item);
+        })
+        child.appendChild(finish);
+        thelist.appendChild(child);
+        theinput.value="";
+
     }
     else{
-        val="";
-        theinput.setAttribute("placeholder","We Need A Text Here");
-    }
+        theinput.value="";
+        theinput.placeholder="Text please ...";
+
+       
+    
+}})
+resetbutton.addEventListener("click", function re(){
+    theinput.value="";
+    thelist.innerHTML="";
+    
 })
-resetbutton.addEventListener("click", function resetall(){
-    theinput = "";
-    thelist="";
-}
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
